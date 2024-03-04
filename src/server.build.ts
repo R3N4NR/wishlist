936e85a98e6cfbcdb3055f09774d5189525ec30d
R3N4NR <renanramalhou@gmail.com>
2024-03-03 23:21:25 -0300

    add build test

---
+++ src/server.build.ts
@@ -0,0 +1,38 @@
+import express, {Request, Response, NextFunction} from 'express';
+import 'express-async-errors';
+import {router} from './routes';
+import {Pool} from 'pg';
+
+const port = 3333;
+const host = '0.0.0.0'
+const app = express();
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'wishlist',
+    password: '1234',
+    port: 5432,
+})
+
+app.use(express.json());
+
+app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message,
+
+        })
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server Error'
+    })
+})
+
+app.listen(port, host, async () => {
+    console.log(`Server rodando na porta : (${port})`);
+
+})
